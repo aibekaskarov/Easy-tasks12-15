aaed8a8f7b9544aa97eb676746b05c92cc7636e7
Aibek <aibekaskarov006@mail.ru>
2023-07-03 16:03:47 +0300

    Easy tasks

---
+++ easy.js
@@ -0,0 +1,76 @@
+//1
+let num = 42
+console.log(num)
+
+//2
+var str = "Hello, world!"
+console.log(str)
+
+//3
+let isTrue = true
+console.log(isTrue)
+
+//4
+a = 5
+b = 3
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+
+//5 
+const pi = 3.14
+console.log(pi)
+
+//6
+let name = "Aibek"
+console.log(name)
+
+//7
+let age = 16
+console.log(age)
+
+//8
+let massage = "Welcome to my website"
+console.log(massage)
+
+//9
+x = 10 
+y = 5
+console.log(x + y)
+console.log(x - y)
+console.log(x / y)
+console.log(x * y)
+
+//10 
+let firstName = "Aibek"
+let lastName = "Askarov"
+console.log(firstName, lastName)
+
+//11
+let width = 5
+let heigth = 8
+console.log(width * heigth)
+
+//12
+let greeting = "Hello"
+let name1 = "Aibek"
+console.log(greeting + " " + name)
+
+//13
+let radius = 5
+let r = 3.14 * radius * radius
+console.log(r)
+
+//14
+let num1 = 10
+let num2 = 3
+let area = num1 / num2
+console.log(area)
+
+//15
+let num3 = 7 
+let num4 = 2
+let num5 = 7 % 2
+console.log(num5)
+
